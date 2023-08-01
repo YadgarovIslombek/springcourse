5ba85b03ffc07bed79af661341de73436b3db8b6
Islombek <64156252+YadgarovIslombek@users.noreply.github.com>
2023-08-01 15:49:50 +0500

    Initial commit

---
+++ src/main/java/uz/islombek/springcourse/SpringBootAppFirstApplication.java
@@ -0,0 +1,13 @@
+package uz.islombek.springcourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAppFirstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAppFirstApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uz/islombek/springcourse/SpringBootAppFirstApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.islombek.springcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAppFirstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
